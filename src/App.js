0ff1e550cca04fbb7369ae12882f79c70d59f257
Samy <samy.belarbi@3wa.io>
2022-08-11 16:22:49 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Error404 from "./pages/Error404";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<Error404 />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
---
+++ src/pages/About.js
@@ -0,0 +1,47 @@
+import React from "react";
+import Logo from "../components/Logo";
+import Navigation from "../components/Navigation";
+
+const About = () => {
+  return (
+    <div>
+      <Logo />
+      <Navigation />
+      <h1>A propos</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Est eaque
+        magnam quas aspernatur quibusdam! Deserunt temporibus in facilis ea
+        obcaecati qui, nesciunt voluptatibus ad ipsa quibusdam laudantium error
+        ipsam, excepturi fugiat aliquid incidunt itaque, odio illum recusandae!
+        Natus ducimus dolores veniam odio rem ipsa et, numquam obcaecati dolore
+        ex consectetur perspiciatis nulla tempora adipisci, distinctio error
+        laborum, unde magni exercitationem fuga voluptatem nemo quas esse amet.
+        Suscipit, aspernatur repellat voluptate quibusdam, deleniti nulla
+        mollitia officiis pariatur unde ipsum eaque quis veritatis ipsa est enim
+        necessitatibus tempore fugit ad, sit facere. Repellendus nihil numquam,
+        perspiciatis praesentium quis in eum laboriosam autem. Delectus vero
+        natus adipisci tenetur. Eveniet itaque ratione illum? In culpa error
+        earum nulla, inventore provident odit doloremque! In fugiat ipsam
+        provident voluptatum sequi aspernatur expedita excepturi blanditiis
+        iusto quidem, similique commodi et delectus ratione. Adipisci facere nam
+        magni unde quam dolore, itaque nulla, et, pariatur rerum eaque nostrum
+        recusandae consequuntur? Obcaecati dolores adipisci assumenda quidem
+        deserunt, magni dicta perferendis omnis voluptatibus consectetur
+        provident doloremque aut. Quas totam modi cupiditate. Eligendi officia
+        eaque repudiandae nulla modi rerum, voluptate a adipisci libero expedita
+        voluptates magnam exercitationem fuga cum, odio ad in quisquam ut
+        aperiam sequi at sit asperiores, facere impedit. Id!
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, ex
+        dolorem dicta qui, eligendi, saepe commodi iure enim placeat ipsa
+        numquam! Nesciunt, ipsum repellat quod quidem sint modi dolorem
+        suscipit.
+      </p>
+    </div>
+  );
+};
+
+export default About;
